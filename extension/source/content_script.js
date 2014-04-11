4d44d191caf1c040d30d6924676485ea2d390e91
Joezo <joe.warren@holidayextras.com>
2014-04-11 08:54:27 +0100

    Replaces words better and supports words ending in ering and ers now.

--- extension/source/content_script.js
+++ extension/source/content_script.js
@@ -24,7 +24,7 @@ startupr.walk = function(node)
       break;
 
     case 3: // Text node
-            if(node.parentElement.tagName.toLowerCase() != "script") {
+            if(node.parentElement.tagName.toLowerCase() != "script" && node.parentElement.tagName.toLowerCase() != 'style') {
                 startupr.handleText(node);
             }
       break;
@@ -34,9 +34,19 @@ startupr.walk = function(node)
 startupr.handleText = function(textNode) {
   var v = textNode.nodeValue;
 
-  v = v.replace(/\b[A-Za-z]*(er)(\s|\.|\,|$)/g, function(match, p1, offset, string) {
+  v = v.replace(/\b[A-Za-z]*(er)(?!;|})\b/g, function(match, p1, offset, string) {
     var s = match.substring(0, match.length - 2);
     return s + 'r';
+  });
+
+  v = v.replace(/\b[A-Za-z]*(ering)(?!;|})\b/g, function(match, p1, offset, string) {
+    var s = match.substring(0, match.length - 5);
+    return s + 'ring';
+  });
+
+  v = v.replace(/\b[A-Za-z]*(ers)(?!;|})\b/g, function(match, p1, offset, string) {
+    var s = match.substring(0, match.length - 3);
+    return s + 'rs';
   });
   textNode.nodeValue = v;
 };
